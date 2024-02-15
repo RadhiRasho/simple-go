c41551cf282f04637187795c40cf3563565518e7
Radhi Rasho <Radhi.H@outlook.com>
2024-02-15 17:53:25 +0000

    simple http request to the board api and type/struct safety

--- https/request.go
+++ https/request.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Activity struct {
+	Activity      string `json:"activity"`
+	Type          string `json:"type"`
+	Participants  float64  `json:"participants"`
+	Price         float64  `json:"price"`
+	Link          string `json:"link"`
+	Key           string `json:"key"`
+	Accessibility float64  `json:"accessibility"`
+}
 
 func main() {
-	fmt.Println("hello")
+	activity := new(Activity);
+
+	data := Get(&activity)
+
+	fmt.Printf("%+v\n", data)
+}
+
+func Get(target interface{}) Activity {
+    r, err := http.Get("https://www.boredapi.com/api/activity")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer r.Body.Close()
+
+    body, err := io.ReadAll(r.Body);
+
+	if err != nil {
+		log.Fatal(err);
+	}
+
+	data := UnmarshalActivity(body)
+
+	return data;
+}
+
+func UnmarshalActivity(data []byte) Activity {
+	var r Activity
+	err := json.Unmarshal(data, &r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return r
+}
+
+func (r *Activity) Marshal() ([]byte, error) {
+	return json.Marshal(r)
 }
