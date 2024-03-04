0d4db4c63a00b66cba0fcc785aaddcbae07c8a0d
Radhi Rasho <Radhi.H@outlook.com>
2024-03-04 13:31:36 +0000

    expirementing with gin gonic

---
+++ gin-server/main.go
@@ -0,0 +1,10 @@
+package main
+
+
+func main() {
+	// Basic Gin Gonic Example from: https://raw.githubusercontent.com/gin-gonic/examples/master/basic/main.go
+	// BasicGinExample()
+
+	// MRE: Minimum Reproducable Example
+	Example()
+}
