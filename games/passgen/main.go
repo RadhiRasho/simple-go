723f5290e016e9e33ef1b6e0fe0104dae08b35f7
Radhi Rasho <Radhi.h@outlook.com>
2024-06-05 14:24:57 +0000

    Updated passgen

--- games/passgen/main.go
+++ games/passgen/main.go
@@ -15,6 +15,12 @@ func main() {
 	var length int
 	flag.IntVar(&length, "length", 8, "The length of the password")
 
+	var numProbability float64 = 0.2;
+	flag.Float64Var(&numProbability, "numProbability", 0.2, "The probability of a number being included in the password")
+
+	var specProbability float64 = 0.2;
+	flag.Float64Var(&specProbability, "specProbability", 0.2, "The probability of a special character being included in the password")
+
 	flag.Parse()
 
 	if length < 8 {
@@ -22,18 +28,28 @@ func main() {
 		os.Exit(1)
 	}
 
-	password := ""
+	if numProbability < 0 || numProbability > 1 {
+		fmt.Println("Number probability must be between 0 and 1")
+		os.Exit(1)
+	}
+
+	if specProbability < 0 || specProbability > 1 {
+		fmt.Println("Special character probability must be between 0 and 1")
+		os.Exit(1)
+	}
+
+	var password string
 
 	for i := 0; i < length; i++ {
 		password += string(letters[rand.Intn(len(letters))])
 
-		// Append a number with 20% probability
-		if rand.Float32() < 0.2 {
+		// Append a number with N% probability (defaults to 20%)
+		if rand.Float64() < numProbability {
 			password += string(numbers[rand.Intn(len(numbers))])
 		}
 
-		// Append a special character with 20% probability
-		if rand.Float32() < 0.2 {
+		// Append a special character with N% probability (defaults to 20%)
+		if rand.Float64() < specProbability {
 			password += string(specials[rand.Intn(len(specials))])
 		}
 	}
